8ca76e818445f739a4f15ca0b53766124e5aff26
hammad ullah <hammadu696@gmail.com>
2024-09-23 11:35:29 +0500

    Add files via upload

---
+++ map1.js
@@ -0,0 +1,266 @@
+const geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.184291, 33.546297]
+            },
+            "properties": {
+                "fillLevel": 100
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.184798, 33.545460]
+            },
+            "properties": {
+                "fillLevel": 30
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.185014, 33.545053]
+            },
+            "properties": {
+                "fillLevel": 65
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.184447, 33.547615]
+            },
+            "properties": {
+                "fillLevel": 85
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.1841, 33.54655]
+            },
+            "properties": {
+                "fillLevel": 33
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18426, 33.54631]
+            },
+            "properties": {
+                "fillLevel": 20
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18442, 33.54608]
+            },
+            "properties": {
+                "fillLevel": 56
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18414, 33.54713]
+            },
+            "properties": {
+                "fillLevel": 71
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18431, 33.54743]
+            },
+            "properties": {
+                "fillLevel": 2
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18454, 33.54782]
+            },
+            "properties": {
+                "fillLevel": 36
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.1853, 33.54776]
+            },
+            "properties": {
+                "fillLevel": 44
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18493, 33.54826]
+            },
+            "properties": {
+                "fillLevel": 12
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18366, 33.54837]
+            },
+            "properties": {
+                "fillLevel": 100
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18577, 33.54782]
+            },
+            "properties": {
+                "fillLevel": 85
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18623, 33.54735]
+            },
+            "properties": {
+                "fillLevel": 20
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18591, 33.54828]
+            },
+            "properties": {
+                "fillLevel": 10
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.1853, 33.54672]
+            },
+            "properties": {
+                "fillLevel": 90
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18518, 33.54961]
+            },
+            "properties": {
+                "fillLevel": 30
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18634, 33.54927]
+            },
+            "properties": {
+                "fillLevel": 41
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18682, 33.54824]
+            },
+            "properties": {
+                "fillLevel": 33
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18612, 33.54685]
+            },
+            "properties": {
+                "fillLevel": 16
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18512, 33.54609]
+            },
+            "properties": {
+                "fillLevel": 73
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18429, 33.55006]
+            },
+            "properties": {
+                "fillLevel": 60
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18713, 33.54744]
+            },
+            "properties": {
+                "fillLevel": 11
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18465, 33.54806]
+            },
+            "properties": {
+                "fillLevel": 69
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [73.18343, 33.54595]
+            },
+            "properties": {
+                "fillLevel": 12
+            }
+        }
+    ]
+};
+
